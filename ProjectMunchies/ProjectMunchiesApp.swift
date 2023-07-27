305f088631745b97434bddd56f71f2c60a964818
dotZ3R0 <drewe7192@gmail.com>
2023-07-27 00:08:42 -0400

    Initial Commit

---
+++ ProjectMunchies/ProjectMunchiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMunchiesApp.swift
+//  ProjectMunchies
+//
+//  Created by DotZ3R0 on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMunchiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
