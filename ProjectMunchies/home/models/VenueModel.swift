ffe428ab4e743dfd8b46655c78ef3a886eded339
dotZ3R0 <drewe7192@gmail.com>
2023-11-25 10:48:50 -0500

    Cleaning up again

---
+++ ProjectMunchies/home/models/VenueModel.swift
@@ -0,0 +1,18 @@
+//
+//  VenueModel.swift
+//  ProjectMunchies
+//
+//  Created by DotZ3R0 on 11/23/23.
+//
+
+import SwiftUI
+
+struct VenueModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VenueModel()
+}
