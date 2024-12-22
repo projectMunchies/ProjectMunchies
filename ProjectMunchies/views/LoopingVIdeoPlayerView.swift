d3c5e2b99c3f7ee666b24f53b608442e7bf14a8f
dotZ3R0 <drewe7192@gmail.com>
2024-12-22 11:50:54 -0500

    some clean up

---
+++ ProjectMunchies/views/LoopingVIdeoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  LoopingVIdeoPlayerView.swift
+//  ProjectMunchies
+//
+//  Created by DotZ3R0 on 9/8/24.
+//
+
+import SwiftUI
+
+struct LoopingVIdeoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoopingVIdeoPlayerView()
+}
---
+++ ProjectMunchies/views/venueTabsView.swift
@@ -0,0 +1,18 @@
+//
+//  venueTabsView.swift
+//  ProjectMunchies
+//
+//  Created by DotZ3R0 on 9/9/24.
+//
+
+import SwiftUI
+
+struct venueTabsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    venueTabsView()
+}
